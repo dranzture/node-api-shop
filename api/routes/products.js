14189476e01fe618dcba0e4c27d61b3db2defb0b
dranzture <polatcoban89@hotmail.com>
2018-07-02 21:11:24 -0500

    chapter 7 done

--- api/routes/products.js
+++ api/routes/products.js
@@ -11,16 +11,27 @@ const Product = require('../models/product');
 
 router.get('/',(req,res,next)=>{
     Product.find()
+    .select('name price _id')
     .exec()
     .then(docs=>{
-        console.log(docs);
-        res.status(200).json(docs);
-        // if(docs.length>=0){
-        //     res.status(200).json(docs);
+        const response ={
+            count: docs.length,
+            products: docs.map(doc => {
+                return{
+                    name: doc.name,
+                    price: doc.price,
+                    _id: doc._id,
+                    request:{
+                        type:'GET',
+                        url:'http://localhost:3000/products/' + doc._id
+                    }
+                }
+            })
+        }
+        // if(doc){
+            res.status(200).json(response);
         // }else{
-        //     res.status(404).json({
-        //         message:'No entries found'
-        //     });
+        //     res.status(404).json({message: 'No product w given ID!'});
         // }
     })
     .catch(err=>{
@@ -30,11 +41,18 @@ router.get('/',(req,res,next)=>{
 
 router.get('/:productID',(req,res,next)=>{
     const productID = req.params.productID;
-    Product.findById(productID).exec()
+    Product.findById(productID)
+    .select('name price _id')
+    .exec()
     .then(doc=>{
-        console.log('From database: ' + doc);
         if(doc){
-            res.status(200).json(doc);
+            res.status(200).json({
+                product: doc,
+                request:{
+                    type:'GET',
+                    url:'http://localhost:3000/products/'+doc._id
+                }
+            });
         }else{
             res.status(404).json({message: 'No product w given ID!'});
         }
@@ -57,7 +75,15 @@ router.post('/',(req,res,next)=>{
         console.log(result);
         res.status(201).json({
             message: "product is created!",
-            createdProduct : product
+            createdProduct : {
+                name: result.name,
+                price: result.price,
+                _id: result._id,
+                request:{
+                    type:'POST',
+                    url:'http://localhost:3000/products/' + result.id
+                }
+            }
         });
     }).catch(err=>{
         console.log(err);
@@ -76,8 +102,13 @@ router.patch('/:productID',(req,res,next)=>{
     Product.update({_id:productID}, {$set: updateOps})
     .exec()
     .then(result=>{
-        console.log(result);
-        res.status(200).json(result);
+        res.status(200).json({
+            message: 'Product updated',
+            request:{
+                type:'PATCH',
+                url:'http://localhost:3000/'+ productID
+            }
+        });
     })
     .catch(err=>{
         console.log(err);
@@ -89,7 +120,13 @@ router.delete('/:productID',(req,res,next)=>{
     Product.findByIdAndRemove({_id: id})
     .exec()
     .then(result=>{
-        res.status(200).json(result);
+        res.status(200).json({
+            message:'Product deleted',
+            ashley: "Loves polat",
+            req:{
+                really: 'something really dont matter that much'
+            }
+        });
     })
     .catch(err=>{
         console.log(err);
